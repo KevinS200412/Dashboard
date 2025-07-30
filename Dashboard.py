b58a41097f57a2cfc17026bb6ae8f7d268f7f9b8
lasanders1 <lucas32c@gmail.com>
2025-07-30 20:03:31 +0200

    hele kleine edits

--- Dashboard.py
+++ Dashboard.py
@@ -15,7 +15,7 @@ ENGLISH_WORDS = set(w.upper() for w in words.words())
 ENGLISH_WORDS.update({
     'ARE', 'CAN', 'DID', 'DO', 'DOES', 'GETS', 'GOES', 'HAD', 'HAS',
     'HAVE', 'IS', 'MAY', 'MUST', 'SAYS', 'WAS', 'WERE', 'WILL', 'PAYS',
-    'WTF', 'WWW', 'DD', 'VS', 'PRE', 'USA'
+    'WTF', 'WWW', 'DD', 'VS', 'PRE', 'USA', 'CPA'
 })
 
 reddit = praw.Reddit(
@@ -314,5 +314,3 @@ def render_page(_, settings):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
